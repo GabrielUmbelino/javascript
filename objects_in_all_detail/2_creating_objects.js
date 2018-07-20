849196afc0c85549624ef0555bb8585bc8886a33
Unknown <gabriel@colorbit.com.br>
2018-07-19 23:02:42 -0300

    objects in all detail

---
+++ objects_in_all_detail/2_creating_objects.js
@@ -0,0 +1,22 @@
+// This is an empty object initialized using the object literal notation
+var myBooks = {};
+
+// This is an object with 4 items, again using object literal
+var mango = {
+  color: "yellow",
+  shape: "round",
+  sweetness: 8,
+
+  howSweetAmI: function() {
+    console.log("Hmm Hmm Good");
+  }
+};
+
+var mango = new Object();
+mango.color = "yellow";
+mango.shape = "round";
+mango.sweetness = 8;
+mango.howSweetAmI = function() {
+    console.log("hmmm good");
+};
+
